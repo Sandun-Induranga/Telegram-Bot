6fe16f554bbc36892e91d8427fab115e2fb92422
Sandun Induranga <88975401+Sandun-Induranga@users.noreply.github.com>
2023-01-16 00:49:05 +0530

    Content Updated

--- responses.py
+++ responses.py
@@ -15,3 +15,15 @@ def bot_responses(message):
 
     if user_message in ('who made you', 'who made you?', 'Who created you', 'Who created you?'):
         return 'Sandun Induranga Created me.'
+
+    if user_message in ('java 8 doc', 'java 8 documentation'):
+        return "https://docs.oracle.com/javase/8/docs/api/index.html"
+
+    if user_message in ('java 11 doc', 'java 11 documentation'):
+        return "https://docs.oracle.com/javase/8/docs/api/index.html"
+
+    if user_message in ('java 17 doc', 'java 17 documentation'):
+        return "https://docs.oracle.com/javase/8/docs/api/index.html"
+
+    if user_message in ('java 18 doc', 'java 18 documentation'):
+        return "https://docs.oracle.com/javase/8/docs/api/index.html"
