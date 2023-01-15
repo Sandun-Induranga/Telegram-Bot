67145eab7ed7e4b6bd943d905c521ef356ad5e91
Sandun Induranga <88975401+Sandun-Induranga@users.noreply.github.com>
2023-01-16 00:53:07 +0530

    Content Updated

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+import Constants as key
+from telegram.ext import *
+import responses as res
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+"""
+* @author : Sandun Induranga
+* @since : 0.1.0
+"""
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
